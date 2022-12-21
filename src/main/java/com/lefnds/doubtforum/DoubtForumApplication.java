7b0c3ab30a0128e8e6df0b790bb08586be886fce
Edu <luizeduardofnds@gmail.com>
2022-12-20 21:32:03 -0300

    Project structure

---
+++ src/main/java/com/lefnds/doubtforum/DoubtForumApplication.java
@@ -0,0 +1,13 @@
+package com.lefnds.doubtforum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoubtForumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoubtForumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lefnds/doubtforum/DoubtForumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lefnds.doubtforum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoubtForumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
