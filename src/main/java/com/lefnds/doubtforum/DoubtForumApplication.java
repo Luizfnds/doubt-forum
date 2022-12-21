c08e3a9733612d356d97688c0e4c4141986ea37b
Edu <luizeduardofnds@gmail.com>
2022-12-21 12:11:42 -0300

    User inMemory

--- src/main/java/com/lefnds/doubtforum/DoubtForumApplication.java
+++ src/main/java/com/lefnds/doubtforum/DoubtForumApplication.java
@@ -2,12 +2,23 @@ package com.lefnds.doubtforum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DoubtForumApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DoubtForumApplication.class, args);
+	}
+
+	@GetMapping
+	@RequestMapping(value = "/test")
+	String test() {
+		return "Test successful!";
 	}
 
 }
