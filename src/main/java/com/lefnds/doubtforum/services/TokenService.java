486c1ef79abd5b2ec5715eceb468ad5318637515
Edu <luizeduardofnds@gmail.com>
2022-12-29 13:26:33 -0300

    Generating JWT token

--- src/main/java/com/lefnds/doubtforum/services/TokenService.java
+++ src/main/java/com/lefnds/doubtforum/services/TokenService.java
@@ -3,20 +3,22 @@ package com.lefnds.doubtforum.services;
 import com.lefnds.doubtforum.model.User;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.stereotype.Service;
 
 import java.util.Date;
 
+@Service
 public class TokenService {
 
-    private static final Long expirationTime = 1800000L;
-    private static final String key = "key";
+    private static final long expirationTime = 1800000;
+    private String key = "key for creation of token";
 
     public String generateToken(User user) {
         return Jwts.builder()
-                .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setSubject("JWT test")
-                .setExpiration(new Date(System.currentTimeMillis() + expirationTime))
-                .signWith(SignatureAlgorithm.HS256, key)
+                .setIssuedAt( new Date( System.currentTimeMillis() ) )
+                .setSubject( "JWT test" )
+                .setExpiration( new Date( System.currentTimeMillis() + expirationTime ) )
+                .signWith( SignatureAlgorithm.HS256 , key )
                 .compact();
     }
 }
--- src/main/java/com/lefnds/doubtforum/services/UserAuthenticationService.java
+++ src/main/java/com/lefnds/doubtforum/services/UserAuthenticationService.java
@@ -5,23 +5,31 @@ import com.lefnds.doubtforum.exceptions.InvalidLoginException;
 import com.lefnds.doubtforum.model.DadosLogin;
 import com.lefnds.doubtforum.model.User;
 import com.lefnds.doubtforum.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserAuthenticationService {
 
-    private final UserRepository repository;
+    private final UserRepository userRepository;
+    private final TokenService tokenService;
 
-    public UserAuthenticationService(UserRepository repository) {
-        this.repository = repository;
+    @Autowired
+    public UserAuthenticationService( UserRepository userRepository , TokenService tokenService ) {
+        this.userRepository = userRepository;
+        this.tokenService = tokenService;
     }
 
     public User auth(DadosLogin dados) {
-        User user = repository.findByEmail(dados.getEmail()).orElseThrow(ExistingEmailException::new);
-        if(user.getPassword().equals(dados.getPassword())) {
+
+        User user = userRepository.findByEmail( dados.getEmail() ).orElseThrow( ExistingEmailException::new );
+        if( dados.getPassword().equals( user.getPassword() ) ) {
+            String token = tokenService.generateToken( user );
+            System.out.println( token );
             return user;
         } else {
             throw new InvalidLoginException();
         }
+
     }
 }
